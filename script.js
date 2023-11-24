dbdcba911842eedab57057858f8f083b1a0939de
Asif Nahyan Kabir <asif-amir@hotmail.com>
2023-11-24 12:29:20 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// const slideShowElements = document.querySelectorAll(".slideshow-element");
+
+// // console.log(slideShowElements);
+
+// setInterval(() => {
+//     let currentElement = document.querySelector(".current");
+
+//     currentElement.classList.remove("current");
+
+//     currentElement.nextElementSibling.classList.add("current");
+// }, 2000);
